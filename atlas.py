aace5916fb901db19059e48def61c6e65260bef8
cobblecracker <cobblecracker@protonmail.com>
2019-08-03 12:27:10 +0200

    squash me

--- atlas.py
+++ atlas.py
@@ -5,11 +5,12 @@ from nbt import nbt
 from PIL import Image
 from PIL.ImagePalette import ImagePalette
 from collections import defaultdict
+from dataclasses import dataclass
 import colors
 
 map_id_re = re.compile(r"^map_(\d+)$")
 
-SIDE_LEN = [2 ** (7+i) for i in range(0, 5)]
+SIDE_LEN = [2 ** (7 + i) for i in range(0, 5)]
 
 
 class Direction(Enum):
@@ -17,6 +18,67 @@ class Direction(Enum):
     NE = 1
     SW = 2
     SE = 3
+
+
+@dataclass(frozen=True)
+class Bounds:
+    x: int
+    z: int
+    scale: int
+
+    def __post_init__(self):
+        assert self.x % 8 == 0
+        assert self.z % 8 == 0
+        assert self.scale >= 0
+
+    def get_coords(self, scale=None):
+        if scale is None:
+            scale = self.scale
+        return ((self.x + 64) // SIDE_LEN[scale], (self.z + 64) // SIDE_LEN[scale])
+
+    def sub_bounds(self, direction):
+        if self.scale == 0:
+            raise Exception("limit")
+        o = SIDE_LEN[self.scale] // 4
+        if direction == Direction.NW:
+            return Bounds(self.x - o, self.z - o, self.scale - 1)
+        elif direction == Direction.NE:
+            return Bounds(self.x - o, self.z + o, self.scale - 1)
+        elif direction == Direction.SW:
+            return Bounds(self.x + o, self.z - o, self.scale - 1)
+        elif direction == Direction.SE:
+            return Bounds(self.x + o, self.z + o, self.scale - 1)
+
+    def direction_from(self, parent):
+        # assert self.scale + 1 == parent.scale
+        ns = 0 if parent.x > self.x else 2
+        we = 0 if parent.z > self.z else 1
+        return Direction(ns + we)
+
+    def contains(self, other):
+        print(self, "contains?", other)
+        if self.scale < other.scale:
+            return False
+        l, t, r, b = self.rect
+        return l < other.x and r > other.x and t < other.z and b > other.z
+
+    def containing_bounds(self, scale):
+        cx, cz = self.get_coords(scale)
+        x = cx * SIDE_LEN[scale] + SIDE_LEN[scale] // 2 - 64
+        z = cz * SIDE_LEN[scale] + SIDE_LEN[scale] // 2 - 64
+        result = Bounds(x, z, scale)
+        assert result.contains(self)
+        return result
+
+    @property
+    def rect(self):
+        side = SIDE_LEN[self.scale]
+        l = self.x - side // 2
+        t = self.z - side // 2
+        b = t + side
+        r = l + side
+        return (l, t, r, b)
+
 
 CROP_COORDS = {
     Direction.NW: (0, 0, 64, 64),
@@ -31,6 +93,7 @@ class MinecraftPalette(ImagePalette):
         p = colors.FULL_RGBA_PALETTE
         super().__init__("RGBA", p, len(p))
 
+
 class AbstractMap:
     @property
     def rect(self):
@@ -41,9 +104,6 @@ class AbstractMap:
         b = t + side
         r = l + side
         return (l, t, r, b)
-
-    def get_megaregion_coords(self):
-        return self.get_coords(4)
 
     def get_coords(self, scale):
         r = tuple(c // (SIDE_LEN[scale]) for c in self.center)
@@ -57,88 +117,99 @@ class AbstractMap:
         we = 0 if z_from > z else 1
         return Direction(ns + we)
 
+
 class Map(AbstractMap):
     def __init__(self, filename):
         stem = PurePath(filename).stem
         m = map_id_re.match(stem)
         self.id = int(m.group(1)) if m else None
         self.nbtfile = nbt.NBTFile(filename)
-        self.center = (int(self.nbtfile["data"]["xCenter"].value), int(self.nbtfile["data"]["zCenter"].value))
-        self.scale = int(self.nbtfile["data"]["scale"].value)
+        self.bounds = Bounds(
+            int(self.nbtfile["data"]["xCenter"].value),
+            int(self.nbtfile["data"]["zCenter"].value),
+            int(self.nbtfile["data"]["scale"].value),
+        )
 
-    def extract_image(self):
+    def get_image(self):
         colors = bytes(self.nbtfile["data"]["colors"].value)
         result = Image.frombytes("P", (128, 128), colors)
-        alpha_layer = bytes(bytearray(map(lambda b: 0x00 if b == 0x00 else 0xff ,colors)))
+        alpha_layer = bytes(
+            bytearray(map(lambda b: 0x00 if b == 0x00 else 0xFF, colors))
+        )
         alpha = Image.frombytes("L", (128, 128), alpha_layer)
         result.im.putpalette(*MinecraftPalette().getdata())
         result = result.convert("RGB")
         result.putalpha(alpha)
         return result
-   
+
     def __repr__(self):
-        return "Map(id={}, scale={}, center={}, coords={})".format(self.id, self.scale, self.center, self.get_coords(self.scale))
+        return "Map(id={}, bounds={})".format(self.id, self.bounds)
 
 
 class MapView(AbstractMap):
-    def __init__(self, center, scale, parent):
-        self.center = center
-        self.scale = scale
+    def __init__(self, bounds, parent):
+        self.bounds = bounds
         self.parent = parent
-    
-    def extract_image(self):
-        parent_image = self.parent.extract_image()
-        direction = self.direction_from(self.parent.center)
-        result = parent_image.crop(CROP_COORDS[direction]).resize((128,128),Image.NEAREST)
+
+    def get_image(self):
+        parent_image = self.parent.get_image()
+        direction = self.bounds.direction_from(self.parent.bounds)
+        result = parent_image.crop(CROP_COORDS[direction]).resize(
+            (128, 128), Image.NEAREST
+        )
         # result.show()
         return result
 
     @classmethod
     def from_direction(cls, direction, parent):
-        return cls(sub_center(direction, parent.center, parent.scale), parent.scale - 1, parent)
+        return cls(
+            sub_center(direction, parent.center, parent.scale), parent.scale - 1, parent
+        )
 
 
 class MapStack(AbstractMap):
-    def __init__(self, center, scale):
-        self.center = center
-        self.scale = scale
+    def __init__(self, bounds):
+        self.bounds = bounds
         self.stack = []
-        
-    def extract_image(self):
-        result = Image.new("RGBA", (128, 128), (0,0,0,0))
-        images = (m.extract_image() for m in self.stack)
+
+    def get_image(self):
+        result = Image.new("RGBA", (128, 128), (0, 0, 0, 0))
+        images = (m.get_image() for m in reversed(self.stack))
         for im in images:
             result.alpha_composite(im)
         return result
 
     def add(self, elt):
-        print(self.scale, elt.scale)
-        print(self.center, elt.center)
-        # assert self.center == elt.center
-        assert self.scale == elt.scale
+        assert self.bounds == elt.bounds
         self.stack.append(elt)
 
+    def empty(self):
+        return not self.stack
+
     def __repr__(self):
-        return "MapStack(stack<{}>)".format(self.stack)  
+        return "MapStack(stack<{}>)".format(self.stack)
 
 
 class MapTree:
-    def __init__(self, center, scale):
-        self.center = center
-        self.scale = scale
-        self.stack = MapStack(center, scale)
-        if scale > 0:
+    def __init__(self, bounds):
+        self.bounds = bounds
+        self.stack = MapStack(bounds)
+        if bounds.scale > 0:
             self.children = {}
         else:
             self.children = None
 
     def add(self, elt):
-        if self.scale == elt.scale:
+        print()
+        print(self, elt)
+
+        # print(self.bounds, elt.bounds)
+        if self.bounds == elt.bounds:
             self.stack.add(elt)
-        elif self.scale > elt.scale:
-            d = elt.direction_from(self.center)
+        elif self.bounds.contains(elt.bounds):
+            d = elt.bounds.direction_from(self.bounds)
             if d not in self.children:
-                self.children[d] = MapTree(sub_center(d, self.center, self.scale), self.scale - 1)
+                self.children[d] = MapTree(self.bounds.sub_bounds(d))
             self.children[d].add(elt)
         else:
             raise Exception("how we got here?")
@@ -148,33 +219,36 @@ class MapTree:
             return
         for d in Direction:
             # print(d)
-            child_center = sub_center(d, self.center, self.scale)
+            # child_center = sub_center(d, self.center, self.scale)
             if d not in self.children:
-                self.children[d] = MapTree(child_center, self.scale - 1)
-            self.children[d].add(MapView.from_direction(d, self.stack))
+                self.children[d] = MapTree(self.bounds.sub_bounds(d))
+            self.children[d].add(MapView(self.bounds.sub_bounds(d), self.stack))
             self.children[d].interpolate()
-
-
-
 
     def __repr__(self):
         if self.children is None:
-            return "MapTree({!r})".format(self.stack)
-        return "MapTree({!r}, \nchildren<{}>)".format(self.stack, list(self.children.items()))  
+            return "MapTree({}, {!r})".format(self.bounds, self.stack)
+        return "MapTree({}, {!r}, \nchildren<{}>)".format(
+            self.bounds, self.stack, list(self.children.items())
+        )
 
 
-class Atlas():
+class Atlas:
     """
     Atlas stores minecraft maps in megaregions
     megaregion is size of a biggest map scale: 128x128 chunks
     """
+
     def __init__(self):
         self.megaregions = {}
 
     def add(self, m):
-        if m.get_megaregion_coords() not in self.megaregions:
-            self.megaregions[m.get_megaregion_coords()] = MapTree(m.center, 4)
-        self.megaregions[m.get_megaregion_coords()].add(m)
+        print(m)
+        if m.bounds.get_coords(4) not in self.megaregions:
+            self.megaregions[m.bounds.get_coords(4)] = MapTree(
+                m.bounds.containing_bounds(4)
+            )
+        self.megaregions[m.bounds.get_coords(4)].add(m)
 
     def update(self, *maps):
         for m in maps:
@@ -186,7 +260,7 @@ class Atlas():
 
     def get_dimension(self):
         x, z = tuple(zip(*self.megaregions.keys()))
-        return (abs(max(x) - min(x)) , abs(max(z) - min(z)))
+        return (abs(max(x) - min(x)), abs(max(z) - min(z)))
 
     def get_maps_by_scale(self):
         result = defaultdict(list)
@@ -194,7 +268,7 @@ class Atlas():
         while len(queue) != 0:
             mt = queue.pop()
             if mt.stack:
-                result[mt.scale].append(mt.stack)
+                result[mt.bounds.scale].append(mt.stack)
             # print(list(mt.children.values()))
             if mt.children is not None:
                 queue.extend(list(mt.children.values()))
@@ -212,23 +286,6 @@ def load_maps(paths):
     return [Map(p) for p in paths]
 
 
-def sub_center(direction, center, scale):
-    x, z = center
-    o = SIDE_LEN[scale] // 4
-    if direction == Direction.NW:
-        return (x - o, z - o)
-    elif direction == Direction.NE:
-        return (x - o, z + o)
-    elif direction == Direction.SW:
-        return (x + o, z - o)
-    elif direction == Direction.SE:
-        return (x + o, z + o)
-
-
-def coords_to_center(coords, scale):
-    return tuple(c * SIDE_LEN[scale] + SIDE_LEN[scale] // 2 for c in coords)
-
-
 def render_by_zoom(atlas, path):
     by_scale = atlas.get_maps_by_scale()
     path = Path(path)
@@ -237,13 +294,10 @@ def render_by_zoom(atlas, path):
         if not zoom_path.exists():
             zoom_path.mkdir()
         for m in maps:
-            x, z = m.get_coords(s)
+            x, z = m.bounds.get_coords(s)
             file_path = zoom_path.joinpath(str(x))
             if not file_path.exists():
                 file_path.mkdir()
             file_path = file_path.joinpath(str(z) + ".png")
             print(file_path)
-            m.extract_image().resize((256,256),Image.NEAREST).save(file_path, "PNG")
-
-
-
+            m.get_image().resize((256, 256), Image.NEAREST).save(file_path, "PNG")
